618841fba4efe3c8c508549600a5581a2e44fcd5
Julian Lechner <fraujulian@lechner.top>
2024-05-06 18:16:11 +0200

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -1,12 +1,12 @@
-function checkIpAddress(ip) { 
-    const ipv4Pattern =  
-        /^(\d{1,3}\.){3}\d{1,3}$/; 
-    const ipv6Pattern =  
-        /^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/; 
-    return ipv4Pattern.test(ip) || ipv6Pattern.test(ip); 
+var hostname = window.location.host;
+
+function checkIpAddress(hostname) { 
+    const ipv4Pattern = /^(\d{1,3}\.){3}\d{1,3}$/; 
+    const ipv6Pattern = /^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/; 
+
+    return ipv4Pattern.test(hostname) || ipv6Pattern.test(hostname); 
 } 
 
-var hostname = window.location.host;
-if (checkIpAddress(hostname) == true) {
+if (checkIpAddress(hostname) == false) {
   window.location.replace("https://www."+hostname);
 }
